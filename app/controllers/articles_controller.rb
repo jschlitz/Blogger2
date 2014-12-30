1e80493d5db9421665b715ae452a0173370ea3d9
Jacob Schlitz <jschlitz@gmail.com>
2014-12-29 19:57:35 -0500

    update, and using partials for the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,12 +18,17 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' created."
+
     redirect_to article_path(@article)
   end
 
   def destroy
     a = Article.find(params[:id])
     a.destroy
+
+    flash.notice = "Article '#{a.title}' destroyed."
+
     redirect_to articles_path()
   end
 
@@ -34,6 +39,8 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated."
 
     redirect_to article_path(@article)
   end
